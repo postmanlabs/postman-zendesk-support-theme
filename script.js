80303c9f36624ef9dc9ac8a51477d813d45c31f9
Brandon Castillo <bcastillo.dev@gmail.com>
2023-12-12 15:57:38 -0800

    complete URL substring sanitization

--- script.js
+++ script.js
@@ -644,7 +644,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Notification Banner
-if (window.location.href.includes("support.postman.com")) {
+if (window.location.href.includes("https://support.postman.com")) {
   document.addEventListener('DOMContentLoaded', async function () {
     // Article label to be considered for the alerts
     const label = 'Alert'
